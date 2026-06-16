bb381f6db0b4cc0b7f4c282f2544eb18074cae4c
AamriGuleria <92876738+AamriGuleria@users.noreply.github.com>
2026-06-16 19:02:35 +0530

    question_retrieval_on_resume_context

--- src/services/embeddings.py
+++ src/services/embeddings.py
@@ -1,8 +1,10 @@
 from sentence_transformers import SentenceTransformer
 from sqlalchemy.orm import Session
 from logging import getLogger
+
+from torch import cosine_similarity
 from models.Interview import Question
-from sqlalchemy import select
+from sqlalchemy import select, text
 
 logger = getLogger(__name__)
 
@@ -28,3 +30,37 @@ def create_question_embeddings(self, db: Session):
         db.commit()
     except Exception as ex:
         logger.error(f"Error creating question embeddings: {ex}")
+
+def retrieve_question_by_resume(resume_summary: text, db: Session):
+    try:
+        resume_embedding = model.encode(
+            resume_summary,
+            show_progress_bar=False
+        ).tolist()
+        result = db.execute(
+            select(Question).where(
+                Question.embedding.is_not(None)
+            ).execution_options(stream_results=True)
+        )
+        questions_with_embeddings = [
+            question[0] for question in result
+        ]
+
+        if not questions_with_embeddings:
+            logger.warning("No questions with embeddings found.")
+            return []
+
+        questions_with_similarity = []
+        for question in questions_with_embeddings:
+            similarity = cosine_similarity(resume_embedding, question.embedding)
+            questions_with_similarity.append((question, similarity))
+
+        questions_with_similarity.sort(key=lambda x: x[1], reverse=True)
+
+        top_questions = [q[0] for q in questions_with_similarity[:50]]
+
+        return top_questions
+
+    except Exception as ex:
+        logger.error(f"Error retrieving questions by resume: {ex}")
+        return []
